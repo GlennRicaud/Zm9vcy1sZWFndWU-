7730ee89dc09b6c8b8d7fabbd464f255bef18b51
Glenn Ricaud <gri@enonic.com>
2017-10-13 21:49:08 +0200

    Manifest and icon

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,12 +1,14 @@
 var mustacheLib = require('/lib/xp/mustache');
 var portalLib = require('/lib/xp/portal');
 var router = require('/lib/router')();
-var view = resolve("main.html");
+
+var mainTemplate = resolve("main.html");
+var manifestTemplate = resolve("manifest.json");
 
 router.get('/', function (req) {
-    var appUrl = portalLib.url({path:'/app/systems.rcd.enonic.foosleague'});
+    var appUrl = getAppUrl();
     var baseUrl = endWithSlash(appUrl) ? appUrl.substring(0, appUrl.length - 1) : appUrl;
-    var body = mustacheLib.render(view, {
+    var body = mustacheLib.render(mainTemplate, {
         appUrl: appUrl,
         baseUrl: baseUrl
     });
@@ -17,9 +19,25 @@ router.get('/', function (req) {
     };  
 });
 
+router.get('/manifest.json', function () {
+    var appUrl = getAppUrl();
+    var body = mustacheLib.render(manifestTemplate, {
+        appUrl: appUrl
+    });
+
+    return {
+        body: body,
+        contentType: 'text/html'
+    };
+});
+
 exports.get = function (req) {
     return router.dispatch(req);
 };
+
+function getAppUrl() {
+    return portalLib.url({path:'/app/systems.rcd.enonic.foosleague'});
+}
 
 function endWithSlash(url) {
     return url.charAt(url.length - 1) === '/';
