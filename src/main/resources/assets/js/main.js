dc97d620f876dcfb27d13376095da3d57a63e2ea
Glenn Ricaud <gri@enonic.com>
2017-10-14 19:20:42 +0200

    App skeleton

---
+++ src/main/resources/assets/js/main.js
@@ -0,0 +1,11 @@
+class FoosLeagueApplication extends RcdMaterialSinglePageApplication {
+    constructor() {
+        super('Foos League');
+    }
+    
+    init() {
+        return super.init();
+    }
+}
+
+new FoosLeagueApplication().init().start();
