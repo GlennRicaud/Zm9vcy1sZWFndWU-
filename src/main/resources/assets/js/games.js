ca134ae6185ed4c2d97eabdc8de8495129b41348
Glenn Ricaud <gri@enonic.com>
2017-10-15 14:13:52 +0200

    Latest games

--- src/main/resources/assets/js/games.js
+++ src/main/resources/assets/js/games.js
@@ -95,26 +95,29 @@ class GameListItem extends ListItem {
         super({
             callback: () => RcdHistoryRouter.setState('game', {id: game.id})
         });
-        this.blueImage = new ImageIcon(GameHelper.getImage(game, GameSide.BLUE))
-            .init();
-        this.redImage = new ImageIcon(GameHelper.getImage(game, GameSide.RED))
-            .init();
         this.blueName = new RcdTextDivElement(GameHelper.getName(game, GameSide.BLUE))
             .init()
             .addClass('game-side-name');
         this.redName = new RcdTextDivElement(GameHelper.getName(game, GameSide.RED))
             .init()
             .addClass('game-side-name');
-        this.score = new GameScore(game).init();
+        const blueImage = new ImageIcon(GameHelper.getImage(game, GameSide.BLUE))
+            .init();
+        const redImage = new ImageIcon(GameHelper.getImage(game, GameSide.RED))
+            .init();
+        const score = new GameScore(game).init();
+        this.center = new RcdDivElement().init()
+            .addClass('center')
+            .addChild(blueImage)
+            .addChild(score)
+            .addChild(redImage);
     }
 
     init() {
         return super.init()
             .addClass('game-list-item')
             .addChild(this.blueName)
-            .addChild(this.blueImage)
-            .addChild(this.score)
-            .addChild(this.redImage)
+            .addChild(this.center)
             .addChild(this.redName);
     }
 }
